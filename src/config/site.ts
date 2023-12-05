73556b6cf78e261836c15c4505a46b1d50239eb1
gneiru <noelrohi59@gmail.com>
2023-12-05 16:42:58 +0800

    site name, description

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,10 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Nextjs Boilerplate",
-  description: "Nextjs template with Shadcn-ui and Layout setted up for you!",
-  url: "https://boilerplate.rohi.dev",
+  name: "K-Next",
+  description:
+    "Experience the enchanting world of Korean drama like never before with our cutting-edge app designed for true K-Drama aficionados. Dive into a captivating universe of compelling storylines, heart-fluttering romances, and breathtaking cinematography. Our app is your passport to an extensive library of the latest and classic Korean drama series, all curated to cater to your unique taste. Whether you're a seasoned K-Drama enthusiast or just beginning your journey into this mesmerizing genre, our app promises an unparalleled streaming experience. Immerse yourself in the rich storytelling, vibrant characters, and cultural allure of Korean dramas, all conveniently accessible at your fingertips. Elevate your entertainment experience – download our app today and embark on an unforgettable K-Drama adventure!",
+  url: "https://kd.rohi.dev",
   links: {
     twitter: "https://twitter.com/gneiru",
     github: "https://github.com/gneiru",
@@ -11,7 +12,7 @@ export const siteConfig: SiteConfig = {
   mainNav: [
     {
       title: "home",
-      href: "/",
+      href: "/home",
     },
     {
       title: "search",
