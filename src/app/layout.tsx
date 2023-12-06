302d0cbeca6d3da6d98f477014e748cd82635d9e
gneiru <noelrohi59@gmail.com>
2023-12-06 17:41:57 +0800

    metatags, public files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,59 @@ import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
-const title = siteConfig.name;
-const description = siteConfig.description;
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
 
 export const metadata: Metadata = {
-  title,
-  description,
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Shadcn",
+    "Radix UI",
+    "Tailwind CSS",
+    "Server Components",
+    "Server Actions",
+    "Kdrama",
+    "Korean",
+  ],
+  authors: [
+    {
+      name: "gneiru",
+      url: "https://github.com/gneiru",
+    },
+  ],
+  creator: "gneiru",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    creator: "@gneiru",
+  },
+  icons: {
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
