74da8c75cffbd7dfde857843071daab975336729
gneiru <noelrohi59@gmail.com>
2023-12-09 14:54:33 +0800

    index email in user table

--- src/db/schema/auth.ts
+++ src/db/schema/auth.ts
@@ -12,16 +12,22 @@ import {
 import { mySqlTable } from "./_table";
 import { watchList } from "./main";
 
-export const users = mySqlTable("user", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("emailVerified", {
-    mode: "date",
-    fsp: 3,
-  }).default(sql`CURRENT_TIMESTAMP(3)`),
-  image: varchar("image", { length: 255 }),
-});
+export const users = mySqlTable(
+  "user",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    name: varchar("name", { length: 255 }),
+    email: varchar("email", { length: 255 }).notNull(),
+    emailVerified: timestamp("emailVerified", {
+      mode: "date",
+      fsp: 3,
+    }).default(sql`CURRENT_TIMESTAMP(3)`),
+    image: varchar("image", { length: 255 }),
+  },
+  (table) => ({
+    emailIdx: index("email_idx").on(table.email),
+  })
+);
 
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
