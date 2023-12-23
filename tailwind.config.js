3d33533798705a3e38796c05434af83e548c2411
gneiru <noelrohi59@gmail.com>
2023-12-23 14:08:11 +0800

    fix font vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,6 +56,11 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        mono: ["var(--font-mono)"],
+        sans: ["var(--font-geist-sans)"],
+        heading: ["var(--font-heading)"],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
