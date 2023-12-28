0b0de4a6c8b72763d168bdfb5ebc2144bf2cc7a0
gneiru <noelrohi59@gmail.com>
2023-12-28 09:31:14 +0800

    Add ratelimiter

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,37 @@
-import { geolocation } from "@vercel/edge";
-import { NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+import { formatRelative } from "date-fns";
+import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/watch")) {
-    const geoloc = geolocation(request);
-    console.log({ geoloc });
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.fixedWindow(5, "10s"),
+});
+
+const ignoredPaths = ["/"];
+
+export async function middleware(request: NextRequest) {
+  if (ignoredPaths.includes(request.nextUrl.pathname)) {
+    console.log("Ignoring public path");
+    return NextResponse.next();
   }
-  const response = NextResponse.next();
-  return response;
+
+  const ip = headers().get("x-forwarded-for");
+  const { success, limit, remaining, reset } = await ratelimit.limit(
+    ip ?? "anonymous"
+  );
+  if (!success) {
+    console.log(`Ratelimited, reset in ${formatRelative(reset, new Date())}`);
+    return new NextResponse(
+      `Too many Requests. Try again in ${formatRelative(reset, new Date())}`,
+      { status: 429 }
+    );
+  }
 }
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
