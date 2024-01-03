6bb3638a673b83b9b491aa890e4a75b363f3bc88
gneiru <noelrohi59@gmail.com>
2024-01-03 12:24:36 +0800

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,7 +14,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     "/popular",
     "/search",
   ].map((path) => ({
-    url: `${siteConfig.url}/${path}`,
+    url: `${siteConfig.url}${path}`,
     changeFrequency: "weekly",
     priority: 1,
     lastModified: new Date(),
