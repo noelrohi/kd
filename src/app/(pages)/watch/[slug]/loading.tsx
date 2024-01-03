0a92e924bcb58d06da6da22c7f55ebad5ed08e3e
Rohi <n@rohi.dev>
2024-01-03 10:59:10 +0800

    add watch page's loading.tsx (#17)

---
+++ src/app/(pages)/watch/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "@/components/fallbacks/loading";
+
+export default function WatchLoading() {
+  return (
+    <section className="flex-1 flex justify-center items-center h-full">
+      <Loading />
+    </section>
+  );
+}
