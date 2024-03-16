5023d0f9cf006d65564557524fc7357d23bd6836
gneiru <noelrohi59@gmail.com>
2024-03-16 13:06:32 +0800

    mysql -> pg

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/schema",
-  driver: "mysql2",
+  driver: "pg",
   out: "./src/db",
   dbCredentials: {
-    uri: env.DATABASE_URL,
+    connectionString: env.DATABASE_URL,
   },
   tablesFilter: ["kd_*"],
   verbose: true,
